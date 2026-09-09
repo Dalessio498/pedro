a68b5fb9becf467da6fa8d94ef30704a45625aa7
Marco Antonio Dalessio <amtoniomarco91@gmail.com>
2026-09-09 16:55:29 -0300

    Add files via upload

---
+++ mural-recados/src/main/java/org/example/Recado.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class Recado {
+    private final int id;
+    private final String autor;
+    private final String mensagem;
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Recado(int id, String autor, String mensagem) {
+        this.id = id;
+        this.autor = autor;
+        this.mensagem = mensagem;
+    }
+
+    public String paraJson(){
+        return "{\"id\":" + id +
+                ",\"autor\":\"" + escapar(autor)
+                + "\", \"mensagem\":\"" + escapar(mensagem) + "\"}";
+    }
+
+    private String escapar(String texto){
+        return texto.replace("\\", "\\\\")
+            .replace("\"", "\\\"")
+            .replace("\r", "")
+            .replace("\n", "\\n");
+    }
+
+
+}
